770d3f328669b237140339f8cbfc72e13855d5bc
immortalWebDev <pgbadgujar007@gmail.com>
2024-08-19 01:51:29 +0530

    Added /m3 as new meet

--- my-second-app/data/dummy.js
+++ my-second-app/data/dummy.js
@@ -1,5 +1,3 @@
-// data/dummy-meetups.js
-
 const DUMMY_MEETUPS = [
     {
       id: 'm1',
@@ -14,7 +12,14 @@ const DUMMY_MEETUPS = [
       image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
       address: 'Some address 10, 12345 Some City',
       description: 'This is a second meetup!'
-    }
+    },
+    {
+        id: 'm3',
+        title: 'A Third Meetup',
+        image: 'https://e0.pxfuel.com/wallpapers/904/521/desktop-wallpaper-coldplay-s-mylo-xyloto-tour-launched-xylobands-led-wristbands-worldwide-newswire-coldplay-concert.jpg',
+        address: 'Atlanta 10, 12345 Glaston',
+        description: 'This is myloxyloto tour!'
+      }
   ];
   
   export default DUMMY_MEETUPS;
--- my-second-app/pages/[meetupId]/index.js
+++ my-second-app/pages/[meetupId]/index.js
@@ -1,26 +1,49 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
-import { useRouter } from 'next/router';
 import DUMMY_MEETUPS from '../../data/dummy'
 
-
-function MeetupDetails() {
-  const router = useRouter();
-  const meetupId = router.query.meetupId;
-
-  const meetup = DUMMY_MEETUPS.find((meetup) => meetup.id === meetupId);
-
-  if (!meetup) {
+function MeetupDetails({ meetupData }) {
+  if (!meetupData) {
     return <p>Meetup not found</p>;
   }
 
   return (
     <MeetupDetail
-      image={meetup.image}
-      title={meetup.title}
-      address={meetup.address}
-      description={meetup.description}
+      image={meetupData.image}
+      title={meetupData.title}
+      address={meetupData.address}
+      description={meetupData.description}
     />
   );
+}
+
+export async function getStaticPaths() {
+  // Define the paths to pre-render based on meetupId
+  return {
+    fallback: false,
+    paths: [
+      { params: { meetupId: 'm1' } },
+      { params: { meetupId: 'm2' } },
+      { params: { meetupId: 'm3' } },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // Fetch data for a single meetup based on the meetupId
+  const meetupId = context.params.meetupId;
+
+  // Example of fetching data (static data for now)
+  const meetupData = DUMMY_MEETUPS.find(meetup => meetup.id === meetupId);
+
+  if (!meetupData) {
+    return { notFound: true }; // Return a 404 page if the meetup isn't found
+  }
+
+  return {
+    props: {
+      meetupData,
+    },
+  };
 }
 
 export default MeetupDetails;
